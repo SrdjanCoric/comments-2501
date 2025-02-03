aff09db34a53ca24bea6905d5b85e410a2a6a24c
Srdjan Coric <srdjancoric@MacBook-Pro-2.local>
2025-02-03 22:57:18 +0100

    day_1 done

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import Comments from "./components/Comments";
+import AddCommentForm from "./assets/AddCommentForm";
+
+function App() {
+  return (
+    <div>
+      <Comments />
+      <AddCommentForm />
+    </div>
+  );
+}
+
+export default App;
